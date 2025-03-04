7e0c153bccb0bfa978ff12d3ecc8e73fa21dfe2b
GGKapanadze <doublegkapanadze@gmail.com>
2025-03-05 00:26:36 +0400

    refactor: reworked the error handling

--- src/drupal/json_rpc.ts
+++ src/drupal/json_rpc.ts
@@ -1,11 +1,12 @@
 import { Formatter } from "../cli/format.ts";
 
-type JSOnRPCResponse<K extends string, T> = {
+type JSONRPCResponse<K extends string, T> = {
   jsonrpc: string;
   id: string;
   result: {
     [key in K]: T;
   };
+  error: Record<string, unknown>;
 };
 
 function createJSONRPCClient(url: string, auth?: string) {
@@ -18,7 +19,7 @@ function createJSONRPCClient(url: string, auth?: string) {
   function req<K extends string, T>(
     method: string,
     params?: Record<string, unknown>
-  ): Promise<JSOnRPCResponse<K, T>> {
+  ): Promise<JSONRPCResponse<K, T>> {
     const request = new Request(url, {
       method: "POST",
       body: JSON.stringify({
@@ -31,9 +32,25 @@ function createJSONRPCClient(url: string, auth?: string) {
     });
 
     return fetch(request)
-      .then((response) => response.json())
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        if (data.error) {
+          let message = `Something went while executing the request: ${method}`;
+          if (data.error.data) {
+            message = data.error.data as string;
+          }
+
+          throw new Error(message);
+        }
+
+        return data;
+      })
       .catch((error) => {
         console.error(Formatter.error(error));
+
+        throw error;
       });
   }
 
